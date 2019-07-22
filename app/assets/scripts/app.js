ce5ac08511655ca92efb900d1c4d1e2c9f78ed60
Benjamin Drejer <benjadrejer@gmail.com>
2019-07-22 17:35:18 +0200

    revealOnScroll module using waypoints, and fix eslint for ES6+

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,7 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 const mobileMenu = new MobileMenu();
+const revealFeatureItems = new RevealOnScroll($('.feature-item'), '85%');
+const revealTestimonials = new RevealOnScroll($('.testimonial'), '60%');
